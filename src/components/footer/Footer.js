6a9111fa9b0a3f92eb35beabd5ac400141a997de
Nurzhigit <Nurjigit00a@gmail.com>
2024-07-04 21:14:44 +0500

    Filled about my data for this portfolio

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
           </ul>
           /
           <div className="copyright">
-            <p>© 2022 frontend-dev.com</p>
+            <p>© 2024 frontend-dev.com</p>
           </div>
         </div>
       </div>
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -7,18 +7,18 @@ const Contacts = () => {
         <ul className="content-list">
           <li className="content-list__item">
             <h2 className="title-2">Location</h2>
-            <p>Moscow, Russia</p>
+            <p>Astana, Kazakhstan</p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Telegram / WhatsApp</h2>
             <p>
-              <a href="tel:+79051234567">+7 (905) 123-45-67</a>
+              <a href="tel:+79051234567">+7 (702) 756-60-57</a>
             </p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Email</h2>
             <p>
-              <a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a>
+              <a href="mailto:Nurzhigit00a@gmail.com">Nurzhigit00a@gmail.com</a>
             </p>
           </li>
         </ul>
